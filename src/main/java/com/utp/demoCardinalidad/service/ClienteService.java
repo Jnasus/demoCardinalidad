139d8de6d47538757d158a695dfd3501c155da89
Sayri10 <kattia.orozco.r@gmail.com>
2025-07-04 00:19:38 -0500

    feat: Add Cliente service layer

---
+++ src/main/java/com/utp/demoCardinalidad/service/ClienteService.java
@@ -0,0 +1,35 @@
+package com.utp.demoCardinalidad.service;
+
+import com.utp.demoCardinalidad.model.Cliente;
+import com.utp.demoCardinalidad.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteService implements IClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Override
+    public List<Cliente> findAll() {
+        return clienteRepository.findAll();
+    }
+
+    @Override
+    public Cliente findById(Integer id) {
+        return clienteRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Cliente save(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        clienteRepository.deleteById(id);
+    }
+}
